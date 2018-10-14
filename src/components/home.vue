5d0a4c4e0157c09b19a185f51918ee2c6339ac5c
haylhf <haylhf@163.com>
2018-10-14 11:17:39 +0800

    更改为头像显示效果

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,98 +1,113 @@
-<template xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-bind="http://www.w3.org/1999/xhtml" >
-    <div class="homeDiv" >
-         <div id="root" class="rootDiv" >
-             <div id="container" class="container" >
+<template xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-bind="http://www.w3.org/1999/xhtml">
+    <div class="homeDiv">
+        <div id="root" class="rootDiv">
+            <div id="container" class="container">
 
-             </div >
-         </div >
-    </div >
-</template >
+            </div>
+        </div>
+    </div>
+</template>
 
-<script >
-      var container = null;
-      var indexPiece = 0,
-		      htmlPic = '',
-		      arrayPic = null,
-		      rotate = 0;//40
-      var transform = function (element, value, key) {
-	      key = key || "Transform";
-	      ["Moz", "O", "Ms", "Webkit", ""].forEach(function (prefix) {
-		      element.style[prefix + key] = value;
-	      });
-	      return element;
-      };
-      window.onload = function () {
-	      _this = this;
-	      var $ = function (selector) {
-		      return document.querySelector(selector);
-	      };
-	      // 获取元素
-	      container = $("#container");
-	      container.addEventListener("click", function () {
-		      transform(container, `rotateY(${(1 * rotate * ++indexPiece)}deg)`);
-	      });
-	      if (container != null) {
-		      arrayPic = [1, 2, 3, 4, 5, 6, 7, 8, 9] // load image list
-		      rotate = 360 / arrayPic.length;
-		      //console.log(`arrayPic:${arrayPic.length}`);
-		      for (let i of arrayPic) {
-			      let img = require(`../assets/img/${i}.jpg`);
-			      htmlPic = htmlPic + `<img id="img${i}" src="${img}" />`;
-		      }
-		      container.innerHTML = htmlPic;
-		      var transZ = 30 * arrayPic.length;
-		      arrayPic.forEach(function (i, j) {
+<script>
+    var container = null;
+    var indexPiece = 0,
+        htmlPic = '',
+        arrayPic = null,
+        rotate = 0;//40
+    var transform = function (element, value, key) {
+        key = key || "Transform";
+        ["Moz", "O", "Ms", "Webkit", ""].forEach(function (prefix) {
+            element.style[prefix + key] = value;
+        });
+        return element;
+    };
+    window.onload = function () {
+
+        _this = this;
+        var $ = function (selector) {
+            return document.querySelector(selector);
+        };
+        // 获取元素
+        container = $("#container");
+        container.addEventListener("click", function () {
+            transform(container, `rotateY(${(1 * rotate * ++indexPiece)}deg)`);
+        });
+
+        if (container != null) {
+            arrayPic = [1, 2, 3, 4, 5, 6, 7, 8, 9] // load image list
+            rotate = 360 / arrayPic.length;
+            for (let i of arrayPic) {
+                let img = require(`../assets/img/female.png`);
+                if (i % 2 == 0) {
+                    img = require(`../assets/img/male.png`);
+                }
+                htmlPic = htmlPic + `<div id="div${i}" class="col-center-block text-center" style="background: darkgrey;border:black solid 1px;">
+                <img id="img${i}" src="${img}" style="width: 150px;height: 150px;margin-top: 20px;" />
+                <br/>
+                <br/>
+                <p class="text-center">test${i}</p>
+                <p class="text-center">test${i}</p>
+                </div>`;
+            }
+            container.innerHTML = htmlPic;
+            var transZ = 35 * arrayPic.length;
+            arrayPic.forEach(function (i, j) {
 //			      console.log(`${i} image,  rotation degree:${j * rotate}`);
-			      transform($(`#img${i}`), `rotateY(${j * rotate}deg) translateZ(${transZ}px)`);
-		      });
-		      console.log(JSON.stringify(container.innerHTML))
-	      }
-      };
-      //      function doAnimationTimer () {
-      //          var id = window.setTimeout(()=> {
-      //              if (container != null) {
-      //                  transform(container, `rotateY(${(1 * rotate * ++indexPiece)}deg)`); //改正负确定旋转方向
-      //              }
-      //              clearTimeout(id);
-      //              doAnimationTimer();
-      //          }, 1500);
-      //      }
+                transform($(`#div${i}`), `rotateY(${j * rotate}deg) translateZ(${transZ}px)`);
+            });
+            console.log(JSON.stringify(container.innerHTML))
+        }
+    };
+    //      function doAnimationTimer () {
+    //          var id = window.setTimeout(()=> {
+    //              if (container != null) {
+    //                  transform(container, `rotateY(${(1 * rotate * ++indexPiece)}deg)`); //改正负确定旋转方向
+    //              }
+    //              clearTimeout(id);
+    //              doAnimationTimer();
+    //          }, 1500);
+    //      }
 
 
-      import Vue from 'vue'
-      var _this
-      var currentInterval
-      export default {
-	      name: "home",
-	      components: {},
-	      data () {
-		      _this = this;
-		      return {}
-	      },
-	      methods: {},
-	      computed: {},
-	      filters: {},
-	      created: function () {
-		      _this = this;
+    import Vue from 'vue'
 
-	      },
-	      mounted: function () {
-		      currentInterval = setInterval(function doAnimation() {
-			      var id = window.setTimeout(()=> {
-				      if (container != null) {
-					      transform(container, `rotateY(${(1 * rotate * ++indexPiece)}deg)`); //改正负确定旋转方向
-				      }
-				      clearTimeout(id);
-			      }, 500);
+    var _this
+    var currentInterval
+    export default {
+        name: "home",
+        components: {},
+        data() {
+            _this = this;
+            return {}
+        },
+        methods: {},
+        computed: {},
+        filters: {},
+        created: function () {
+            _this = this;
 
-		      }, 1500);//定时器
-	      },
-	      destroyed: function () {
-		      clearInterval(currentInterval);
-	      }
-      }
+        },
+        mounted: function () {
+            currentInterval = setInterval(function doAnimation() {
+                var id = window.setTimeout(() => {
+                    if (container != null) {
 
-</script >
-<style >
+                        transform(container, `rotateY(${(1 * rotate * ++indexPiece)}deg)`); //改正负确定旋转方向
+                        console.log(`indexPiece: ${indexPiece}`);
+                    }
+                    clearTimeout(id);
+                }, 500);
 
-</style >
+            }, 1500);//定时器
+        },
+        destroyed: function () {
+            clearInterval(currentInterval);
+        }
+    }
+
+</script>
+<style>
+    span {
+        text-align: center;
+    }
+</style>

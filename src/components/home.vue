4ac3646749da72eacc6df1d7ea2d255ec2b386b8
haylhf <haylhf@163.com>
2018-10-15 16:48:21 +0800

    Fix bug, 网页重新激活时，动画阻塞执行的问题。

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,67 +1,82 @@
-<template xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-bind="http://www.w3.org/1999/xhtml">
-    <div>
-        <video :src="getBgVideo()" style="right:0; bottom: 0; z-index: -100;min-width: 100%;min-height: 100%;position: fixed;height: auto;width: auto;-webkit-filter: grayscale(20%)"
-               loop="loop" autoplay="autoplay" muted></video>
-        <div class="homeDiv">
-            <div id="root" class="rootDiv">
-                <div id="container" class="container">
+<template xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-bind="http://www.w3.org/1999/xhtml" >
+    <div >
+        <video :src="getBgVideo()"
+               style="right:0; bottom: 0; z-index: -100;min-width: 100%;min-height: 100%;position: fixed;height: auto;width: auto;-webkit-filter: grayscale(20%)"
+               loop="loop" autoplay="autoplay" muted ></video >
+        <div class="homeDiv" >
+            <div id="root" class="rootDiv" >
+                <div id="container" class="container" >
 
-                </div>
-            </div>
-        </div>
-    </div>
+                </div >
+            </div >
+        </div >
+    </div >
 
-</template>
+</template >
 
-<script>
+<script >
     var container = null;
     var indexPiece = 0,
-        htmlPic = '',
-        arrayPic = null,
-        rotate = 0;//40
+		    htmlPic = '',
+		    arrayPic = null,
+		    rotate = 0;//40
     var transform = function (element, value, key) {
-        key = key || "Transform";
-        ["Moz", "O", "Ms", "Webkit", ""].forEach(function (prefix) {
-            element.style[prefix + key] = value;
-        });
-        return element;
+	    key = key || "Transform";
+	    ["Moz", "O", "Ms", "Webkit", ""].forEach(function (prefix) {
+		    element.style[prefix + key] = value;
+	    });
+	    return element;
     };
+    var isActived = true;
+    window.ondeactivate = ()=> {
+	    isActived = false;
+    };
+    window.onactivate = ()=> {
+	    isActived = true;
+    };
+    window.onblur = ()=> {
+	    isActived = false;
+    };
+    window.onfocus = ()=> {
+	    isActived = true;
+    };
+
     window.onload = function () {
 
-        _this = this;
-        var $ = function (selector) {
-            return document.querySelector(selector);
-        };
-        // 获取元素
-        container = $("#container");
-        container.addEventListener("click", function () {
-            transform(container, `rotateY(${(1 * rotate * ++indexPiece)}deg)`);
-        });
+	    _this = this;
+	    var $ = function (selector) {
+		    return document.querySelector(selector);
+	    };
+	    // 获取元素
+	    container = $("#container");
+	    container.addEventListener("click", function () {
+		    transform(container, `rotateY(${(1 * rotate * ++indexPiece)}deg)`);
+	    });
 
-        if (container != null) {
-            arrayPic = [1, 2, 3, 4, 5, 6, 7, 8, 9] // load image list
-            rotate = 360 / arrayPic.length;
-            for (let i of arrayPic) {
-                let img = require(`../assets/img/female.png`);
-                if (i % 2 == 0) {
-                    img = require(`../assets/img/male.png`);
-                }
-                htmlPic = htmlPic + `<div id="div${i}" class="col-center-block text-center" style="background: darkgrey;border:black solid 1px;">
+	    if (container != null) {
+		    arrayPic = [1, 2, 3, 4, 5, 6, 7, 8, 9] // load image list
+		    rotate = 360 / arrayPic.length;
+		    for (let i of arrayPic) {
+			    let img = require(`../assets/img/female.png`);
+			    if (i % 2 == 0) {
+				    img = require(`../assets/img/male.png`);
+			    }
+			    htmlPic = htmlPic + `<div id="div${i}" class="col-center-block text-center" style="background: darkgrey;border:black solid 1px;">
                 <img id="img${i}" src="${img}" style="width: 150px;height: 150px;margin-top: 20px;" />
                 <br/>
                 <br/>
                 <p class="text-center">test${i}</p>
                 <p class="text-center">test${i}</p>
                 </div>`;
-            }
-            container.innerHTML = htmlPic;
-            var transZ = 35 * arrayPic.length;
-            arrayPic.forEach(function (i, j) {
+		    }
+		    container.innerHTML = htmlPic;
+		    var transZ = 35 * arrayPic.length;
+		    arrayPic.forEach(function (i, j) {
 //			      console.log(`${i} image,  rotation degree:${j * rotate}`);
-                transform($(`#div${i}`), `rotateY(${j * rotate}deg) translateZ(${transZ}px)`);
-            });
-            console.log(JSON.stringify(container.innerHTML))
-        }
+			    transform($(`#div${i}`), `rotateY(${j * rotate}deg) translateZ(${transZ}px)`);
+		    });
+		    console.log(JSON.stringify(container.innerHTML))
+	    }
     };
     //      function doAnimationTimer () {
     //          var id = window.setTimeout(()=> {
@@ -80,44 +95,48 @@
     var currentInterval
     var bg_video = require('../assets/img/bg.mp4');
     export default {
-        name: "home",
-        components: {},
-        data() {
-            _this = this;
-            return {}
-        },
-        methods: {
-            getBgVideo() {
-                return bg_video;
-            }
-        },
-        computed: {},
-        filters: {},
-        created: function () {
-            _this = this;
+	    name: "home",
+	    components: {},
+	    data() {
+		    _this = this;
+		    return {}
+	    },
+	    methods: {
+		    getBgVideo() {
+			    return bg_video;
+		    }
+	    },
+	    computed: {},
+	    filters: {},
+	    created: function () {
+		    _this = this;
 
-        },
-        mounted: function () {
-            currentInterval = setInterval(function doAnimation() {
-                var id = window.setTimeout(() => {
-                    if (container != null) {
+	    },
+	    mounted: function () {
+		    currentInterval = setInterval(function doAnimation() {
+			    if (!isActived) {
+				    return;
+			    }
+			    var id = window.setTimeout(() => {
+				    if (container != null) {
+					    if (isActived) {
+						    transform(container, `rotateY(${(1 * rotate * ++indexPiece)}deg)`); //改正负确定旋转方向
+						    console.log(`indexPiece: ${indexPiece}`);
+					    }
+				    }
+				    clearTimeout(id);
+			    }, 500);
 
-                        transform(container, `rotateY(${(1 * rotate * ++indexPiece)}deg)`); //改正负确定旋转方向
-                        console.log(`indexPiece: ${indexPiece}`);
-                    }
-                    clearTimeout(id);
-                }, 500);
-
-            }, 1500);//定时器
-        },
-        destroyed: function () {
-            clearInterval(currentInterval);
-        }
+		    }, 1500);//定时器
+	    },
+	    destroyed: function () {
+		    clearInterval(currentInterval);
+	    }
     }
 
-</script>
-<style>
+</script >
+<style >
     span {
-        text-align: center;
+	    text-align: center;
     }
-</style>
+</style >
